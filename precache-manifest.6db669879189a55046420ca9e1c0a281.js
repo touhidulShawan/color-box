aefcfacb3e365b30a5c8ca564d0780ad06001cbf
Touhidul Shawan <touhidulshawan@gmail.com>
2019-07-24 15:39:06 +0600

    Updates

---
+++ precache-manifest.6db669879189a55046420ca9e1c0a281.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb8fa9dfdef6e75dd84e456783feeb52",
+    "url": "/color-box/index.html"
+  },
+  {
+    "revision": "93c9935932c56192a8d2",
+    "url": "/color-box/static/css/main.776c097d.chunk.css"
+  },
+  {
+    "revision": "0deeb155c9dba6c41804",
+    "url": "/color-box/static/js/2.1ea1d828.chunk.js"
+  },
+  {
+    "revision": "93c9935932c56192a8d2",
+    "url": "/color-box/static/js/main.e49779e8.chunk.js"
+  },
+  {
+    "revision": "16a838273ed180ab9f34",
+    "url": "/color-box/static/js/runtime~main.47c4d061.js"
+  }
+]);
